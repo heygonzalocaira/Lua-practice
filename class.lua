28a0483012a6d29bc5f29363cddcdcd7bbeb9026
Gonzalo Caira <gc98wootz@gmail.com>
2018-07-28 08:31:04 -0500

    Practicando Lua con RECURSOR youtube
    Se inicia con los primeros videos hasta el video 4

---
+++ class.lua
@@ -0,0 +1,34 @@
+local Class = {}
+
+Class.__index = Class
+
+--default implementation
+
+function Class:new() end
+
+
+--create a new Class type from our base class
+
+function Class:derive(type)
+    local cls = {}
+    cls.type = type
+    cls.__index = cls
+    cls.super = self
+    setmetatable(cls,self)
+    return cls
+end
+
+function Class:__call(...)
+    local inst = setmetatable({},self)
+    inst:new(...)
+    return inst
+end
+
+function Class:get_type()
+    return self.type
+end
+
+local Player = Class:derive("player")
+local plyr1 = Player()
+
+return Class
